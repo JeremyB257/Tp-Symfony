efbd41f40741d93b5664121ac8a3e3debca4184b
Jerem <bertelootjerem@gmail.com>
2023-03-15 15:23:49 +0100

    add fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+
+        //add Event
+        for ($i = 0; $i < 20; $i++) {
+            $product = new Event();
+            $product->setTitle('concert' . $i)
+                ->setDescription($faker->text())
+                ->setStartDate($faker->dateTimeBetween('now', '+15 jours'))
+                ->setEndDate($faker->dateTimeBetween('+16jours', '+30 jours'))
+                ->setPrice(rand(0, 1) == 1 ? rand(10, 50) : null);
+
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
